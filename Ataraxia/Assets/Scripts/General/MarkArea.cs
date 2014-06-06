0d15cc667b7c5a9e4b34651df7c17904029159df
Julian Valdes <julian.cruz07@gmail.com>
2014-06-06 10:19:47 -0500

    add message control

---
+++ Ataraxia/Assets/Scripts/General/MarkArea.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarkArea : MonoBehaviour 
+{
+	[SerializeField]
+	private string name;
+	[SerializeField]
+	private Color color;
+	[SerializeField]
+	private Transform parent;
+	[SerializeField]
+	private Vector3 area;
+
+	private void OnDrawGizmos () 
+	{
+		Gizmos.color = color;
+		Gizmos.DrawWireCube(parent.position, area);		
+	}
+}
---
+++ Ataraxia/Assets/Scripts/UI/Controls/AtaraxiaText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtaraxiaText : SpriteText 
+{
+	protected override void Awake ()
+	{
+		base.Awake ();
+	}
+	public override void Start ()
+	{
+		base.Start ();
+	}
+}
