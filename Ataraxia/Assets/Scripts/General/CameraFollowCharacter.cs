03fa9f72d9a271a065e15b238acb30d02075ebae
Julian Valdes <julian.cruz07@gmail.com>
2014-06-11 00:57:09 -0500

    create scene to test board

---
+++ Ataraxia/Assets/Scripts/General/CameraFollowCharacter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowCharacter : MonoBehaviour 
+{
+	[SerializeField]
+	private float distance = 10.0F;
+	[SerializeField]
+	private Transform target;
+	[SerializeField]
+	private Transform myTransform;
+
+	private void Update ()
+	{
+		myTransform.position = new Vector3(target.position.x, myTransform.position.y, target.position.z - distance);
+		myTransform.LookAt(target);
+	}
+}
