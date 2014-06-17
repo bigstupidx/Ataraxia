fdf795ec57930f5b7b5a7a2a0f467db4c33c2ed2
juliancruz87 <julian.cruz07@gmail.com>
2014-06-17 18:09:00 -0500

    escondidas mini game

--- Ataraxia/Assets/Scripts/CharacterScripts/AnimationManager.cs
+++ Ataraxia/Assets/Scripts/CharacterScripts/AnimationManager.cs
@@ -5,7 +5,7 @@ public class AnimationManager
 {
 	public const string IDLE = "Idle";
 	public const string CELEBRATION = "Celebration";
-	public const string COUNT = "Couting";
+	public const string COUNT = "Counting";
 	public const string JUMP_DICE = "Jump_Dice";
 	public const string LOSE = "Lose";
 	public const string RUN = "Run";
@@ -20,7 +20,6 @@ public class AnimationManager
 
 	public Animation Play (string animationName)
 	{
-		Debug.Log (animationName + " : " + animation.IsPlaying(animationName));
 		if(!animation.IsPlaying(animationName))
 			animation.Play(animationName);
 
---
+++ Ataraxia/Assets/Scripts/MiniGames/Hidden/HiddenManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiddenManager : MonoBehaviour 
+{
+	[SerializeField]
+	private GameObject blackScreen;
+	[SerializeField]
+	private Character character;
+	[SerializeField]
+	private Timer time;
+	[SerializeField]
+	private Transform cameraTransform;
+	[SerializeField]
+	private AtaraxiaText readyText;
+	private bool canStartGame;
+
+	private void Start ()
+	{
+		StartGame ();
+		readyText.Hide (true);
+	}
+
+	public void StartGame ()
+	{
+		blackScreen.SetActive (true);
+		time.StartTimer ();
+		character.LookAt(cameraTransform.position);
+		character.StartCounting ( time.TotalTime );
+	}
+
+	private void Update ()
+	{
+		if(time.IsTimeOver && !canStartGame )
+		{
+			canStartGame = true;
+			readyText.Hide (false);
+			Invoke( Helpers.NameOf ( HideBlackScreen ) , 1.5F);
+		}
+	}
+
+	private void HideBlackScreen ()
+	{
+		character.RestartRotation ();
+		readyText.Hide (true);
+		blackScreen.SetActive ( false);
+	}
+}
