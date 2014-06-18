4b54e987d5cdb307b1fe58019ee7e38317fd7370
Julian Valdes <julian.cruz07@gmail.com>
2014-06-18 00:14:10 -0500

    add window to clear player prefs

---
+++ Ataraxia/Assets/Editor/Ataraxia/CleanUpWindow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;  
+using UnityEditor;  
+using System.Collections;  
+using System.Collections.Generic;  
+
+public class CleanUpWindow : EditorWindow  
+{  
+	[MenuItem("Window/CleanUpWindow")]
+	static void Init()
+	{ 
+		EditorWindow.GetWindow(typeof(CleanUpWindow));
+	}
+	
+	void OnGUI()
+	{ 
+		if(GUILayout.Button("Clear PlayerPrefs"))
+			PlayerPrefs.DeleteAll ();
+	}  
+}
